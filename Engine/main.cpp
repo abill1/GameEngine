d1250eb66fd1e8542a6a6928c4b676281a6e03d1
Anthony Bill <36425922+abill1@users.noreply.github.com>
2024-09-09 23:51:12 -0500

    Added some ability to respond to left mouse button inputs and redraw the ellipse. Added a console to be used for debugging. Issue exists when passing a float as an argument. The float arguments are not appended to the formatted string.

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -9,6 +9,8 @@
 #include <tchar.h>
 #include <assert.h>
 #include <d2d1.h>
+#include <windowsx.h>
+#include <cstdio>
 
 //================================================================================
 //----- Macros
@@ -29,12 +31,17 @@ ID2D1Factory* p2dFactory;
 ID2D1HwndRenderTarget* p2dRenderTarget;
 ID2D1SolidColorBrush* pBrush;
 D2D1_ELLIPSE ellipse;
+D2D1_POINT_2F ptMouse;
+HANDLE hStdout;
+DWORD cWritten;
 
 //================================================================================
 //----- Function Declarations
 //================================================================================
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+D2D1_POINT_2F PixelsToDips(HWND _hwnd, float _x, float _y);
+void ConsoleLog(const wchar_t* Text, ...);
 
 //================================================================================
 //----- Main
@@ -44,9 +51,18 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLin
 {
 	UNUSED_VAR(hPrevInstance);
 	UNUSED_VAR(pCmdLine);
+	const wchar_t CLASS_NAME[] = L"WindowClass";
+
+	// ----- Create a Console for Window App
+	AllocConsole();
+	ConsoleLog(L"Hello World\n");
+	ConsoleLog(L"It was nice to meet you. I hope to see you again!\n");
+	float price = 0.99f;
+	ConsoleLog(L"I would like to buy %d %s from you today for $ %d .\n", 5, L"apples", 3);
+	
 
 	// ----- Register the window class.
-	const wchar_t CLASS_NAME[] = L"WindowClass";
+	
 	WNDCLASS wc = { };
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
@@ -86,12 +102,51 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLin
 		}
 	}
 
+	FreeConsole();
+
 	return 0;
 }
 
 //================================================================================
 //----- Functions Defines
 //================================================================================
+
+void ConsoleLog(const wchar_t* Text, ...)
+{
+#if _DEBUG
+	const int MAX_BUF_SIZE = 4096;
+	static char CONSOLE_BUFFER[MAX_BUF_SIZE];
+	memset(CONSOLE_BUFFER, 0, MAX_BUF_SIZE);
+	static wchar_t WIDE_CONSOLE_BUF[MAX_BUF_SIZE];
+	memset(WIDE_CONSOLE_BUF, 0, MAX_BUF_SIZE);
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	DWORD cwr = {};
+
+	// ----- Read in any variable arguments
+	va_list args;
+	va_start(args, Text);
+	wvsprintf((LPWSTR)WIDE_CONSOLE_BUF, Text, args); // TODO: Figure out issue with float args not being passed. 
+	va_end(args);
+
+	// ----- Write to console
+	int len = WideCharToMultiByte(CP_UTF8, 0, WIDE_CONSOLE_BUF, lstrlen(WIDE_CONSOLE_BUF), nullptr, 0, nullptr, nullptr);
+	if (len > MAX_BUF_SIZE)
+		len = MAX_BUF_SIZE;
+	WideCharToMultiByte(CP_UTF8, 0, WIDE_CONSOLE_BUF, lstrlen(WIDE_CONSOLE_BUF), CONSOLE_BUFFER, len, nullptr, nullptr);
+	WriteFile(hOut, CONSOLE_BUFFER, len, &cwr, nullptr);
+
+	// ----- Write to Visual Studio Output
+	OutputDebugStringW(WIDE_CONSOLE_BUF);
+#endif
+}
+
+D2D1_POINT_2F PixelsToDips(HWND _hwnd, float _x, float _y)
+{
+	float dpi = (float)GetDpiForWindow(_hwnd);
+	float scale = dpi / 96.0f;
+	return(D2D1::Point2F(_x / scale, _y / scale));
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -102,7 +157,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		if (FAILED(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &p2dFactory)))
 			result =  -1;
-		break;// return 0;
+		break;
 	}
 	case WM_DESTROY:
 	{
@@ -117,7 +172,40 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		bRunning = false;
 		// ----- Close window
 		PostQuitMessage(0);
-		break;// return 0;
+		break;
+	}
+	case WM_LBUTTONDOWN:
+	{
+		SetCapture(hwnd);
+		ptMouse = PixelsToDips(hwnd, (float)GET_X_LPARAM(lParam), (float)GET_Y_LPARAM(lParam));
+		ellipse.point = ptMouse;
+		ellipse.radiusX = 1.0f;
+		ellipse.radiusY = 1.0f;
+		printf("Mouse Point: (X:{%.2f},Y:{%.2f})\n ", ptMouse.x, ptMouse.y);
+		InvalidateRect(hwnd, nullptr, FALSE);
+		break;
+	}
+	case WM_LBUTTONUP:
+	{
+		ReleaseCapture();
+		break;
+	}
+	case WM_MOUSEMOVE:
+	{
+		float pixelx = (float)GET_X_LPARAM(lParam);
+		float pixely = (float)GET_Y_LPARAM(lParam);
+		DWORD flags = (DWORD)wParam;
+		if (flags & MK_LBUTTON)
+		{
+			D2D1_POINT_2F dips = PixelsToDips(hwnd, pixelx, pixely);
+			float width = (dips.x - ptMouse.x) / 2.0f;
+			float height = (dips.y - ptMouse.y) / 2.0f;
+			float x1 = ptMouse.x + width;
+			float y1 = ptMouse.y + height;
+			ellipse = D2D1::Ellipse(D2D1::Point2F(x1, y1), width, height);
+			InvalidateRect(hwnd, nullptr, FALSE);
+		}
+		break;
 	}
 	case WM_PAINT:
 	{
@@ -126,7 +214,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			RECT rc = {};
 			GetClientRect(hwnd, &rc);
-			D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+			D2D1_SIZE_U size = D2D1::SizeU((UINT32)rc.right, (UINT32)rc.bottom);
 
 			hr = p2dFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(hwnd, size), &p2dRenderTarget);
 			if (SUCCEEDED(hr))
@@ -164,16 +252,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			}
 			EndPaint(hwnd, &ps);
 		}
-
-		//PAINTSTRUCT ps;
-		//HDC hdc = BeginPaint(hwnd, &ps);
-
-		// All painting occurs here, between BeginPaint and EndPaint.
-
-		//FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-		//EndPaint(hwnd, &ps);
-		break;// return 0;
+		break;
 	}
 	case WM_SIZE:
 	{
@@ -182,7 +261,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			RECT rc;
 			GetClientRect(hwnd, &rc);
 
-			D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+			D2D1_SIZE_U size = D2D1::SizeU((UINT32)rc.right, (UINT32)rc.bottom);
 
 			p2dRenderTarget->Resize(size);
 			if (p2dRenderTarget != NULL)
@@ -195,11 +274,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			}
 			InvalidateRect(hwnd, NULL, FALSE);
 		}
-		break;// return 0;
+		break;
 	}
 	default:
 		result = DefWindowProc(hwnd, uMsg, wParam, lParam);
-
+		break;
 	}
 	return result;
 }
