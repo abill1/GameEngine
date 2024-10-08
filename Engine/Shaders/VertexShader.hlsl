e4f18efad633e713c66ed2e1d44a3edcaf3accd7
Anthony Bill <36425922+abill1@users.noreply.github.com>
2024-10-07 20:55:09 -0500

    Added a constant buffer to store the transformation matrix to be used with instancing.

--- Engine/Shaders/VertexShader.hlsl
+++ Engine/Shaders/VertexShader.hlsl
@@ -17,10 +17,15 @@ cbuffer imageBuffer : register(b0)
 	float4x4 imgCoord;
 };
 
-outData main( inData input )
+cbuffer trsBuffer : register(b1)
+{
+	float4x4 TRS[16];
+};
+
+outData main( inData input, uint id : SV_instanceID  )
 {
 	outData output;
-	output.pos = input.pos;
+	output.pos = mul(TRS[id], input.pos);
 	output.color = input.color;
 	float4 tc = float4(input.texcoord[0], input.texcoord[1], 0.0f, 1.0f);
 	float4 rtc = mul(imgCoord, tc);
