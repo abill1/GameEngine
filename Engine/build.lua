5261a8025b8954740bd4274f2b2d94d03f308f92
Anthony Bill <36425922+abill1@users.noreply.github.com>
2024-10-19 14:04:25 -0500

    Updated build file to include the static libraries and the header file for FreeType.

--- Engine/build.lua
+++ Engine/build.lua
@@ -10,10 +10,12 @@ links {
 	"d3d11.lib",
 	"d3dcompiler.lib",
 	"winmm.lib",
-	"Dxgi.lib"
+	"Dxgi.lib",
+	"freetype.lib"
 }
 libdirs {
 	"%{wks.location}/build/bin/%{cfg.architecture}-%{cfg.buildcfg}/",
+	"Vendor/FreeType/x64_libs/%{cfg.buildcfg}/"
 }
 
 postbuildcommands {
