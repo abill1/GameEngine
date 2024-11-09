966fc13a5684e02dac2827f20cde3d39427a5137
Anthony Bill <36425922+abill1@users.noreply.github.com>
2024-11-09 11:19:54 -0600

    Added framework file for includes, typedefs, and macros used throughout the codebase, and use as a force include.

---
+++ Engine/Framework/Framework.h
@@ -0,0 +1,32 @@
+
+#ifndef FRAMEWORK_H
+#define FRAMEWORK_H
+
+#include <wrl/client.h>
+#include <DirectXMath.h>
+#include <minwinbase.h>
+#include <assert.h>
+
+//================================================================================
+//----- Macros
+//================================================================================
+
+#define CHECK_NULL(x) assert(x!=nullptr)
+#define CHECK_EQUALS(x,y) assert(x==y)
+#define CHECK_NOT(x,y) assert(x!=y)
+#define UNUSED_VAR(x) (void(x))
+#define COMPTR(T, x) Microsoft::WRL::ComPtr<T> x;
+#define RELEASECOM(x) if(x!=nullptr){x->Release();x=nullptr;}
+#define CLEARMEM(x) ZeroMemory(&x, sizeof(x))
+
+//================================================================================
+//----- Typedefs
+//================================================================================
+
+typedef DirectX::XMFLOAT4 fVect;
+typedef DirectX::XMFLOAT3 fVect3;
+typedef DirectX::XMFLOAT2 fVect2;
+
+#endif // FRAMEWORK_H
+
+
